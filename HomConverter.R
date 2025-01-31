8c8213035282fe50d83a00bf4fef342f834e5435
KYU <existence.m.p@gmail.com>
2025-01-31 18:45:04 +0900

    initial commit

---
+++ HomConverter.R
@@ -0,0 +1,86 @@
+# --------------------------------------------------------------------------------------------------
+#
+#   MIT License
+# 
+#   Copyright (c) 2025 KYU @ https://github.com/KYU49
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+#   and associated documentation files (the "Software"), to deal in the Software without 
+#   restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+#   distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all copies or 
+#   substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+#   BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+#   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#   You should have received a copy of the The MIT License
+#   along with this program.  If not, see <https://opensource.org/license/mit>.
+#
+# --------------------------------------------------------------------------------------------------
+
+HomConverter <- setRefClass(
+    Class = "HomConverter",
+    fields = list(
+        mouseDB = "data.frame",
+        humanDB = "data.frame"
+    ),
+    methods = list(
+        initialize = function(rptFile = "HOM_MouseHumanSequence.rpt"){
+            m2h <- read.table(rptFile, sep="\t", header=T, comment.char = "")
+            mouseDB <<- m2h[m2h$NCBI.Taxon.ID == 10090, c("DB.Class.Key", "EntrezGene.ID", "Symbol")]
+            humanDB <<- m2h[m2h$NCBI.Taxon.ID == 9606, c("DB.Class.Key", "EntrezGene.ID", "Symbol")]
+        },
+        converter = function(fromDB, toDB, id, allow_multiple_hit){
+            notFouond <- list(entrezGeneId = NA_integer_, symbol = "NA")
+            classKey <- fromDB[fromDB$EntrezGene.ID == id, "DB.Class.Key"]
+            if(length(classKey) == 0) {
+                return(return(notFouond))
+            }
+            results <- toDB[toDB$DB.Class.Key == classKey[1], c("EntrezGene.ID", "Symbol")]
+            if(length(results) == 0) {
+                return(notFouond)
+            }
+
+            if(allow_multiple_hit){
+                return(
+                    list(
+                        entrezGeneId = results$EntrezGene.ID,
+                        symbol = results$Symbol
+                    )
+                )
+            }
+            return(
+                list(
+                    entrezGeneId = results$EntrezGene.ID[1],
+                    symbol = results$Symbol[1]
+                )
+            )
+        },
+        mouse2human = function(mouseEntrezId, allow_multiple_hit = FALSE){
+            if(length(mouseEntrezId) > 1){
+                return(
+                    lapply(mouseEntrezId, function(id) {
+                        converter(mouseDB, humanDB, id, allow_multiple_hit)
+                    })
+                )
+            }
+            return(converter(mouseDB, humanDB, mouseEntrezId, allow_multiple_hit))
+        },
+        human2mouse = function(humanEntrezId, allow_multiple_hit = FALSE){
+            if(length(mouseEntrezId) > 1){
+                return(
+                    lapply(humanEntrezId, function(id) {
+                        converter(humanDB, mouseDB, id, allow_multiple_hit)
+                    })
+                )
+            }
+            return(converter(humanDB, mouseDB, humanEntrezId, allow_multiple_hit))
+        }
+    )
+)
---
+++ README.md
@@ -0,0 +1,59 @@
+** The English text in the README.md is a machine translation from Japanese. **
+
+# About
+このツールは、マウスとヒトの遺伝子間でホモロジーのある遺伝子のEntrez Gene ID (GSEA解析などで使われる)を相互に変換するRのスクリプトです。
+
+This tool is an R script that converts the Entrez Gene IDs of homologous genes between mouse and human (used in analyses such as GSEA).
+
+# Requirements
+* Access the following URL:  
+  http://www.informatics.jax.org/homology.shtml
+* Download the file named "HOM_MouseHumanSequence.rpt" from the link,  
+  "Complete List of Human and Mouse Homologs (plain text)" 
+* Place it in the same directory as the script.
+
+# Usage
+## Initialization
+```R
+source("HomConverter.R")
+conv <- HomConverter("HOM_MouseHumanSequence.rpt")    # arg is path of the rpt file.
+```
+
+## Convert EntrezGeneID from Mouse to Human
+```R
+result <- conv$mouse2human(Your_Interest_EntrezGeneID_of_mouse)
+print(result$entrezGeneId)    # If input is `107747` (EntrezGeneID of Aldh1l1), output is `[1] 10840`
+print(reulst$symbol)          # If input is `107747`, output is `[1] ALDH1L1`
+```
+
+## Convert EntrezGeneID from Human to Mouse
+```R
+result <- conv$human2mouse(Your_Interest_EntrezGeneID_of_human)
+print(result$entrezGeneId)
+print(reulst$symbol)
+```
+
+## Convert list of EntrezGeneIDs from Mouse to Human
+```R
+result <- conv$mouse2human(c(id1, id2, id3, ...))
+print(result[[1]]$entrezGeneId) # EntrezGeneID of Human converted from id1
+```
+
+## Not 1:1 but 1:many convert
+
+### `66483` is EntrezGeneID for the mouse gene Rpl36al, while in humans, it corresponds to the gene RPL36A and RPL36AL.
+```R
+result <- conv$mouse2human(66483, allow_multiple_hit = TRUE)
+print(result$symbol)        # `[1] "RPL36A"  "RPL36AL"`
+print(result$entrezGeneId)  # `[1] 6173 6166`
+print(result$symbol[1])     # `[1] "RPL36A"`
+```
+
+### If `allow_multiple_hit` is FALSE (default), the first one that is found will be returned as the output.
+```R
+result <- conv$mouse2human(66483)
+print(result$symbol)     # `[1] "RPL36A"`
+```
+
+# License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
